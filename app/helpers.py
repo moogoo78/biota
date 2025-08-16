83f7c1e26c1af83213bf338cd790860101beec97
moogoo <moogoo78@gmail.com>
2025-08-17 01:14:07 +0800

    fix: null taicol_name_id

--- app/helpers.py
+++ app/helpers.py
@@ -198,7 +198,7 @@ def get_namespace_data(namespace_id):
         distribution = ''
         add_fields = {}
         specimens = []
-        taicol_name_id = None
+        taicol_name_id = row['id']
 
         per_usages = []
         type_specimens = {}
@@ -226,7 +226,6 @@ def get_namespace_data(namespace_id):
         #        data['literatures'].append(x)
 
         if x:= row['group']:
-            # taicol_name_id = x
             # mysql_cursor.execute(f"SELECT ru.id, ru.status, t.name, t._authorship, r.title FROM reference_usages ru LEFT JOIN `references` r ON r.id = ru.reference_id LEFT JOIN taxon_names t ON t.id = ru.taxon_name_id WHERE ru.accepted_taxon_name_id={x} and ru.status != 'accepted'")
             # results = mysql_cursor.fetchall()
             # sci_name = ''

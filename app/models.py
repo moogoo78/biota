0c950aa6df1a4b721dfbabf00738318f59bd9d92
moogoo <moogoo78@gmail.com>
2025-12-23 10:37:04 +0800

    fix: ItemSynonym add schema length

--- app/models.py
+++ app/models.py
@@ -435,8 +435,8 @@ class ItemSynonym(Base):
     __tablename__ = 'item_synonym'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(500))
-    ref: Mapped[str] = mapped_column(String(500))
+    name: Mapped[str] = mapped_column(String(1000))
+    ref: Mapped[str] = mapped_column(String(1000))
     item_id: Mapped[int] = mapped_column(ForeignKey('item.id'))
     item: Mapped[Item] = relationship(back_populates='synonyms')
 
